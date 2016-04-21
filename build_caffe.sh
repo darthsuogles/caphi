ee7a81f3857f31ecf26b02e9a51666be626a037e
Philip Yang <philip@quantifind.com>
2016-04-21 01:03:33 -0700

    successfully built with build_caffe.sh

--- build_caffe.sh
+++ build_caffe.sh
@@ -9,8 +9,6 @@ function _log_msg() {
 function quit_with() { _log_msg "ERROR" "$@"; exit; }
 function log_info() { _log_msg "INFO" "$@"; }
 
-[ "${OS}" == "darwin" ] || quit_with "only support OSX for the moment"
-
 cp Makefile.config.example _tmp_Makefile.config
 
 function set_build_option() {
@@ -30,14 +28,17 @@ function del_build_option() {
 
 set_build_option "CPU_ONLY" "1"
 set_build_option "USE_LMDB" "1"
+set_build_option "USE_LEVELDB" "0"
 set_build_option "BLAS" "openblas"
 set_build_option "OPENCV_VERSION" "3"
 set_build_option "CUSTOM_CXX" "clang++"
+set_build_option "WITH_PYTHON_LAYER" "1"
 
 # Before removing, combine configs spanning multiple lines
-perl -i.bak -pe 's@\\\s*$@@sm' _tmp_Makefile.config
+perl -i.bak -pe 's@^(.*)\s*\\$@$1`#=LNCMB=#`\\@m' _tmp_Makefile.config
+perl -i.bak -pe 's@\\\s*$@@m' _tmp_Makefile.config
 
-del_build_option "CUDA_ARCH"
+#del_build_option "CUDA_ARCH"
 del_build_option "CUDA_DIR"
 del_build_option "USE_CUDNN"
 
@@ -46,3 +47,45 @@ del_build_option "PYTHON_LIB"
 del_build_option "TEST_GPUID"
 del_build_option "ANACONDA_HOME"
 del_build_option "MATLAB_DIR"
+del_build_option "INCLUDE_DIRS"
+del_build_option "LIBRARY_DIRS"
+
+# Restore previously combined lines
+perl -i.bak -pe 's@`#=LNCMB=#`@\\\n@g' _tmp_Makefile.config
+
+# Add some additional configs to the end
+[ "${OS}" == "darwin" ] || quit_with "only support OSX for the moment"
+brew install gflags glog pkg-config
+#brew install boost --c++11
+brew install boost-python --c++11 --with-python3 --without-python
+brew install hdf5 --c++11
+brew install protobuf --c++11 --devel
+brew tap homebrew/science
+brew install openblas tbb
+brew install opencv3 --with-opengl --with-qt5 --with-tbb --with-java --c++11 --with-python3
+
+numpy_core_root="$(dirname "$(python3 -c 'import numpy.core; print(numpy.core.__file__)')")"
+py3_root="$(python3-config --prefix)"
+py3_incs="${py3_root}/Headers ${numpy_core_root}/include"
+py3_libs="${py3_root}/lib ${numpy_core_root}/lib"
+cv3_root="$(brew --prefix opencv3)"
+
+#export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:$(brew --prefix python3)/lib/pkgconfig"
+py3_libname="$(python3-config --libs | perl -ne 'print $1 if /-l(python[^\s]*)(\.so|\.dylib)*/')"
+
+cat <<EOF >> _tmp_Makefile.config
+# Additional libraries and include dirs
+PYTHON_LIBRARIES := boost_python3 ${py3_libname}
+PYTHON_INCLUDE := ${py3_incs}
+PYTHON_LIB := ${py3_libs}
+
+INCLUDE_DIRS := ${py3_incs} $(brew --prefix)/include ${cv3_root}/include
+LIBRARY_DIRS := ${py3_libs} $(brew --prefix)/lib ${cv3_root}/lib $(brew --prefix boost-python)/lib
+EOF
+
+
+# Now proceed to building
+mv _tmp_Makefile.config Makefile.config
+rm -f _tmp_Makefile.config.bak
+make all -j32
+DISTRIBUTE_DIR=$PWD/install_dir make distribute
