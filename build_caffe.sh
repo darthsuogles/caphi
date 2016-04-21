52339cc0bede7801fe3c3a1ec6e042bc72749c5d
Philip Yang <philip@quantifind.com>
2016-04-20 22:52:01 -0700

    Remove multi-line build options

--- build_caffe.sh
+++ build_caffe.sh
@@ -21,7 +21,28 @@ function set_build_option() {
 	 _tmp_Makefile.config
 }
 
+function del_build_option() {
+    local optName=$1
+    perl -i.bak -pe \
+	 "s/^\s*#?\s*${optName}\s+:=\s+.*(\\\\.+)*//sg" \
+	 _tmp_Makefile.config    
+}
+
 set_build_option "CPU_ONLY" "1"
 set_build_option "USE_LMDB" "1"
 set_build_option "BLAS" "openblas"
 set_build_option "OPENCV_VERSION" "3"
+set_build_option "CUSTOM_CXX" "clang++"
+
+# Before removing, combine configs spanning multiple lines
+perl -i.bak -pe 's@\\\s*$@@sm' _tmp_Makefile.config
+
+del_build_option "CUDA_ARCH"
+del_build_option "CUDA_DIR"
+del_build_option "USE_CUDNN"
+
+del_build_option "PYTHON_INCLUDE"
+del_build_option "PYTHON_LIB"
+del_build_option "TEST_GPUID"
+del_build_option "ANACONDA_HOME"
+del_build_option "MATLAB_DIR"
